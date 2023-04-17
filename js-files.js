9301b824781f848b52f825e0a072a8c2aa56563b
dandutra <danieldutra06@gmail.com>
2023-04-17 19:36:10 -0300

    Link slider input to grid output. Rework css grid property. Add comments

--- js-files.js
+++ js-files.js
@@ -1,55 +1,67 @@
-/* Etch-a-Sketch project:
-1- The page will have a grid created by javascript, this grid's size is inputable.
-2- The grid will be made with a series of divs.
-3- There will be a reset button to the grid. The button will listen to a click and reset the grid.*/
-
-// JAVASCRIPT GRID
-    function createGrid(rows, columns) {
-    const gridContainer = document.querySelector('#grid-container');
-    for(let i = 0; i < rows; i++) {
-        for(let j = 0; j < columns; j++) {
-            const cellDiv = document.createElement('div');
-            cellDiv.className = "cell";
-            cellDiv.style.gridTemplateColumns = `repeat(${rows},${columns}fr)`;
-            gridContainer.appendChild(cellDiv);
-        }
+  
+  function createGrid(rows, columns) {
+    let gridContainer = document.querySelector('#grid-container');
+    let slider = document.getElementById("slider");
+    let cellSize = 50 / columns;
+  
+    //GRID SETUP
+    slider.addEventListener("input", updateGrid);
+    gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    gridContainer.style.gridAutoRows = `${cellSize}fr`; 
+    gridContainer.style.gridAutoColumns = `${cellSize}fr`;
+  
+    //GRID CREATOR
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        const cellDiv = document.createElement('div');
+        cellDiv.className = "cell";
+        gridContainer.appendChild(cellDiv);
+        //cell event
+        cellDiv.addEventListener('mouseover', function () {
+          colorCell(cellDiv);
+        });
+      }
     }
-}
-createGrid(16,16);
+  }
 
-//CELL EVENTS
-const cellDiv = document.querySelectorAll('.cell');
-cellDiv.forEach(function(cell) {
-  cell.addEventListener('mouseover', function() {
-    colorCell(cell);
-  });
-});
+  function updateGrid() {
+    let slider = document.getElementById("slider");
+    let gridContainer = document.getElementById("grid-container");
+      gridContainer.innerHTML = "";
+      
+      let rows = slider.value;
+      let columns = slider.value;
+      createGrid(rows, columns);
+    }
 
-function colorCell(cell) {
-  cell.style.backgroundColor = "black";
-}
+  function colorCell(cell) {
+    cell.style.backgroundColor = "black";
+  }
 
-const reset = document.querySelector('.reset-button');
-reset.addEventListener('click', discolorCell);
-
-function discolorCell () {
+  //RESET DIV
+  const reset = document.querySelector('.reset-button');
+  reset.addEventListener('click', discolorCell);
+  
+  function discolorCell() {
     const cellDiv = document.querySelectorAll('.cell');
     cellDiv.forEach(cellDiv => {
-        cellDiv.style.backgroundColor = "white";
+      cellDiv.style.backgroundColor = "white";
     })
-}
+  }
+  
+  //ONLOAD SETUP
+  window.onload = function () {
+    let rows = 50;
+    let columns = 50;
+    createGrid(rows, columns);
+  
+    let slider = document.getElementById("slider");
+    slider.addEventListener("input", updateGrid);
+  }
 
-//SLIDER
-
-function getValue() {
-  let slider = document.getElementById("slider");
-  let textInput = document.getElementById("text-input");
-  textInput.value = slider.value;
-
-}
-
-    
-
-
-
-
+  //SLIDER ONINPUT
+  function getValue() {
+    const textInput = document.getElementById("text-input");
+    textInput.value = slider.value;
+  }
+  
